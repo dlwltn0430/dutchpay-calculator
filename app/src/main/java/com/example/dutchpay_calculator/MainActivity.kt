1a209d06d9f37cc593e38abe9fec97ca9c173cb3
Jisu Lee <dlwltn0430@naver.com>
2023-03-12 18:46:38 +0900

    feat: 선택한 라디오버튼에 따라 다른 fragment 보여주는 기능 구현

--- app/src/main/java/com/example/dutchpay_calculator/MainActivity.kt
+++ app/src/main/java/com/example/dutchpay_calculator/MainActivity.kt
@@ -2,10 +2,70 @@ package com.example.dutchpay_calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var fragmentContainer: FrameLayout
+    private lateinit var radioGroup: RadioGroup
+    private lateinit var dutchpayV1: RadioButton
+    private lateinit var dutchpayV2: RadioButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fragmentContainer = findViewById(R.id.fragment_container)
+        radioGroup = findViewById(R.id.radio_group)
+        dutchpayV1 = findViewById(R.id.ver1)
+        dutchpayV2 = findViewById(R.id.ver2)
+
+        radioGroup.setOnCheckedChangeListener { _, checkedId ->
+            when (checkedId) {
+                R.id.ver1 -> showV1()
+                R.id.ver2 -> showV2()
+            }
+        }
+    }
+
+    private fun showV1() {
+        val v1Fragment = V1()
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, v1Fragment)
+            .commit()
+    }
+
+    private fun showV2() {
+        val v2Fragment = V2()
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, v2Fragment)
+            .commit()
+    }
+
+}
+
+class V1 : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.dutchpay_ver1, container, false)
+    }
+}
+
+class V2 : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.dutchpay_ver2, container, false)
     }
 }
--- app/src/main/java/com/example/dutchpay_calculator/dutchpay.java
+++
@@ -1,6 +0,0 @@
-package com.example.dutchpay_calculator;
-
-import androidx.fragment.app.Fragment;
-
-public class dutchpay extends Fragment {
-}
